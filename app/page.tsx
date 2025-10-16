5bb2bb11b33e4fb09600f6c83a2392e0e5a29fd9
221fa04732 <221fa04732@gmail.com>
2025-10-16 12:11:09 +0530

    home landing page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import HomeLanding from "@/components/component/homeLanding";
+import Navbar from "@/components/component/navbar";
+
 export default function Home(){
-    return(<div className="flex items-center justify-center min-h-screen min-w-full text-2xl font-semibold">
-        Welcome to WardStone!
+    return(<div className="flex flex-col items-center justify-start min-h-screen min-w-full">
+        <Navbar />
+        <HomeLanding />
     </div>)
 }
---
+++ components/component/homeLanding.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image"
+
+export default function HomeLanding(){
+    return(<div className="relative w-full md:h-[650px] h-screen">
+        <Image src={'/home.png'} alt="home" fill className="object-cover opacity-50"/>
+        <div className="absolute h-full flex flex-col items-center justify-center">
+            <div className="w-10/12 flex flex-col items-center justify-center gap-16">
+                <div className="flex justify-center items-start gap-2 border border-white/50 text-white/50 rounded-full px-6 py-1">
+                    <div className="bg-white/50 rounded-full max-h-2 max-w-2 min-h-2 min-w-2 mt-2"></div>
+                    <div className="text-center">Next-generation Space Defense Technology</div>
+                </div>
+                <div className="text-3xl md:text-6xl font-bold rubik text-center mt-6">Satellites for Missile<div>Defense</div></div>
+                <div className="text-center md:text-sm text-xs">Wardstone is a space defense-tech company developing next-generation capabilities to protect the United States and its allies from missiles and other space-based threats. We design, build, and deploy satellites equipped with space-based interceptors to kinetically counter hypersonic and ballistic missiles.</div>
+            </div>
+        </div>
+    </div>)
+}
