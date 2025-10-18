c39df349863778bd2a002c101eb91141575826c1
221fa04732 <221fa04732@gmail.com>
2025-10-18 10:47:28 +0530

    contact us content change

--- components/component/contactUs.tsx
+++ components/component/contactUs.tsx
@@ -1,9 +1,8 @@
 export default function ContactUs(){
     return(
         <div className="w-11/12 max-w-[1250px] flex flex-col justify-center items-center gap-2 md:gap-6 md:py-16 py-6 bg-[linear-gradient(135deg,#3883BC_0%,#001B2F_100%)] rounded-2xl mb-16 mt-32">
-            <div className="text-black text-sm md:text-lg font-medium">Interested in Working on the Future of <div>Space Defense and National Security!</div></div>
-            <div className="rubik text-xl md:text-3xl font-semibold">We’re Hiring!</div>
-            <a href="/career#open-role" className="bg-black px-6 py-2 rounded-full mt-6 text-sm md:text-base">See open roles</a>
+            <div className="rubik text-xl md:text-3xl font-semibold text-center">Interested in Investing, Working, <div>or Collaborating with Us?</div></div>
+            <a href="/career#open-role" className="bg-black px-6 py-2 rounded-full mt-6 text-sm md:text-base">Send a message</a>
         </div>
     )
 }
