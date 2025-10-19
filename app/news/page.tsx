56a11dbabcf9c5dbf574113f8c2377cbcce3a271
221fa04732 <221fa04732@gmail.com>
2025-10-19 10:42:24 +0530

    news landing section done

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,9 +1,11 @@
 import Footer from "@/components/component/footer"
 import Navbar from "@/components/component/navbar"
+import NewsLanding from "@/components/component/newsLanding"
 
 export default function News(){
 return(<div className="flex flex-col items-center justify-center min-h-screen min-w-full">
         <Navbar />
+        <NewsLanding />
         <Footer />
     </div>)
 }
