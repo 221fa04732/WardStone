e21fae2716bdb31c3dd61db85c0b00f6f3743e56
221fa04732 <221fa04732@gmail.com>
2025-10-20 13:24:47 +0530

    member desc removed, directly navigate to linkedin

--- components/component/teamCard.tsx
+++ components/component/teamCard.tsx
@@ -1,13 +1,5 @@
 import Image from "next/image"
 import { teamInfoType } from "./teamOverview"
-import {
-  Drawer,
-  DrawerContent,
-  DrawerDescription,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer"
 
 export default function TeamCard({data} : {data : teamInfoType}){
     return(<div className="w-[300px] relative flex flex-col flex-wrap justify-center items-center gap-2 bg-[#090909] rounded-lg px-6">
@@ -16,22 +8,8 @@ export default function TeamCard({data} : {data : teamInfoType}){
         </div>
         <div className="rubik text-xl mt-[100px] text-center">{data.name}</div>
         <div className="text-white/50 text-center">{data.designation}</div>
-        <Drawer>
-            <DrawerTrigger className="border border-blue-500 px-6 py-1.5 rounded-full my-6 cursor-pointer">learn more</DrawerTrigger>
-            <DrawerContent className="flex flex-col justify-center items-center border-t-blue-500 mb-10">
-                <div className="w-11/12 flex flex-col justify-center items-center gap-2">
-                    <DrawerHeader className="flex flex-col justify-center items-center">
-                        <Image src={data.url} alt="team" width={150} height={150} className="rounded-full" />
-                        <DrawerTitle className="rubik">{data.name}</DrawerTitle>
-                        <DrawerDescription>{data.designation}</DrawerDescription>
-                        <div className='flex justify-start items-center gap-4'>
-                            <a href={data.linkedinURL} target='_blank'><Image src={'/linkedin.svg'} alt="discord" height={15} width={15} /></a>
-                            <a href={data.xURL} target='_blank' ><Image src={'/twitter.svg'} alt="discord" height={15} width={15} /></a>
-                        </div>
-                    </DrawerHeader>
-                    <div className="max-w-[900px] text-sm">{data.about}</div>
-                </div>
-            </DrawerContent>
-        </Drawer>
+        <a href={data.linkedinURL} target='_blank' className="my-6">
+            <button className="border border-blue-500 px-6 py-1.5 rounded-full cursor-pointer">learn more</button>
+        </a>
     </div>)
 }
--- components/component/teamOverview.tsx
+++ components/component/teamOverview.tsx
@@ -4,46 +4,34 @@ export type teamInfoType = {
     name : string,
     designation : string,
     url : string,
-    about : string,
-    linkedinURL : string,
-    xURL : string
+    linkedinURL : string
 }
 
 const teamInfo : teamInfoType[] = [{
     name : "Sebastian Fischer",
     designation : "Chief Executive Officer",
     url : "/team1.png",
-    about : "I am the cofounder & CEO of Wardstone, a space defense company building satellites to defend the U.S. and its allies from hypersonic and ballistic missiles. I have 10+ years of engineering and product experience in satellites, drones, and self-driving cars. I held leadership roles at Amazon Prime Air and Cruise Self-Driving and previously worked at Lockheed Martin and NASA on Planetary Spacecraft. I studied Aerospace Engineering at MIT and did a dual MS/MBA degree at Harvard.",
-    linkedinURL : "https://www.linkedin.com/in/sebafischer/",
-    xURL : ""
+    linkedinURL : "https://www.linkedin.com/in/sebafischer/"
 },{
     name : "Tobias Fischer",
     designation : "Chief Technology Officer",
     url : "/team2.png",
-    about : "I am the cofounder of Wardstone, where I lead the technical development of next-generation defense spacecraft. Previously I worked at Astranis, building, launching, and operating geostationary communication satellites. I hold a bachelor's in mechanical engineering from Cornell University. At Cornell, I was the Chief Spacecraft Engineer for a lunar CubeSat mission that demonstrated novel electric propulsion and navigation systems.",
-    linkedinURL : "https://www.linkedin.com/in/tobifischer/",
-    xURL : ""
+    linkedinURL : "https://www.linkedin.com/in/tobifischer/"
 },{
     name : "Gur Kimchi",
     designation : "Advisor",
     url : "/team3.png",
-    about : "",
-    linkedinURL : "",
-    xURL : ""
+    linkedinURL : "https://www.linkedin.com/in/gurkimchi/"
 },{
     name : "Ret. Maj. Gen. Charles Corcoran",
     designation : "Advisor",
     url : "/team4.png",
-    about : "",
-    linkedinURL : "",
-    xURL : ""
+    linkedinURL : "https://www.linkedin.com/in/charles-corcoran-353a6834/"
 },{
     name : "Garry Tan",
     designation : "Partner and Investor",
     url : "/team5.png",
-    about : "",
-    linkedinURL : "",
-    xURL : ""
+    linkedinURL : "https://www.linkedin.com/in/garrytan/"
 }]
 
 export default function TeamOverview(){
