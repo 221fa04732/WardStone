ece4646fb282bde94822f67f9d31fcc05ca845ba
221fa04732 <221fa04732@gmail.com>
2025-10-21 13:43:03 +0530

    created backend route for finding job with id

---
+++ app/api/jobs/[id]/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse, NextRequest } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function GET(req: NextRequest, {params} : { params: { id: string } }) {
+
+    const { id } =await params;
+    const job = await prisma.jobs.findUnique({
+        where : {
+            id : id
+        }
+    })
+    if(!job){
+        return NextResponse.json({ 
+            error: "Job not found" 
+        },{ 
+            status: 404 
+        });
+    }
+    return NextResponse.json({ 
+        job : job
+    });
+}
