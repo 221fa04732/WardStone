96d6bb76d36086038deebd4a2f381274ba459fb9
221fa04732 <221fa04732@gmail.com>
2025-10-21 11:14:23 +0530

    added loader component

---
+++ components/component/loading.tsx
@@ -0,0 +1,10 @@
+import { Spinner } from "../ui/spinner"
+
+export default function Loading({data} : {data : string}){
+    return(<div className="min-h-screen w-full flex justify-center items-center">
+        <div className="w-11/12 flex flex-col justify-center items-center gap-2">
+            <div className="text-center">{data}</div>
+            <Spinner className="size-6 text-blue-500" />
+        </div>
+    </div>)
+}
