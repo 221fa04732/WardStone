229dd8dace0c049ad01f840d9aa5e940ccc609c9
221fa04732 <221fa04732@gmail.com>
2025-10-21 16:19:48 +0530

    added height parameter to error and height comp

--- app/career/[id]/page.tsx
+++ app/career/[id]/page.tsx
@@ -38,11 +38,11 @@ export default function JobDetailPage(){
     },[])
 
     if(loading){
-        return(<Loading data={"Please wait! Fetching job details"} />)
+        return(<Loading data={"Please wait! Fetching job details"} height='mih-h-screen'/>)
     }
 
     if(error){
-        return(<Error data={"Job not found, Please retry!"}/>)
+        return(<Error data={"Job not found, Please retry!"} height='mih-h-screen'/>)
     }
 
     return(<div className='relative overflow-hidden w-full min-h-screen flex flex-col justify-start items-center'>
