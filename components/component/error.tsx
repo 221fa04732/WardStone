c79fe69d0913b202e87cb140e7c87c031b004295
221fa04732 <221fa04732@gmail.com>
2025-10-21 13:16:10 +0530

    error component added

---
+++ components/component/error.tsx
@@ -0,0 +1,8 @@
+export default function Error({data} : {data : string}){
+    return(<div className="min-h-screen w-full flex justify-center items-center">
+        <div className="w-11/12 flex flex-col justify-center items-center gap-2">
+        <div className="text-center">{data}</div>
+            <div className="text-center rubik text-lg md:text-2xl">Oops! Something went wrong</div>
+        </div>
+    </div>)
+}
