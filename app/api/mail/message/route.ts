ed0c126306199bbc3484228f659e7529a520851c
221fa04732 <221fa04732@gmail.com>
2025-10-22 14:22:53 +0530

    added api route for sending message

---
+++ app/api/mail/message/route.ts
@@ -0,0 +1,48 @@
+import { NextRequest, NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+
+export async function POST(req : NextRequest){
+    try{
+        const formData = await req.formData();
+        const name = formData.get("name") as string;
+        const email = formData.get("email") as string;
+        const message = formData.get("message") as string;
+
+        if(!name || !email || !message){
+            return NextResponse.json({ 
+                message: "Missing data fields" 
+            },{ 
+                status: 400 
+            });
+        }
+
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.MY_EMAIL,
+                pass: process.env.MY_EMAIL_PASS,
+            },
+        });
+
+        const mailOptions = {
+            from: email,
+            to: process.env.MY_EMAIL,
+            subject: `New message from ${name}`,
+            text: `Name : ${name}\nEmail : ${email}\n\n${message}`,
+        };
+
+        await transporter.sendMail(mailOptions);
+        return NextResponse.json({ 
+            success: true, 
+            message: "Message send successfully!" 
+        });
+    }
+    catch(e){
+        return NextResponse.json({ 
+            success: false, 
+            message: "Failed to send message" 
+        },{ 
+            status: 500 
+        });
+    }
+}
