87f14477686d14e2df743374bb9a9f9f73736d2d
221fa04732 <221fa04732@gmail.com>
2025-10-22 08:35:52 +0530

    small css fix

--- app/career/[id]/page.tsx
+++ app/career/[id]/page.tsx
@@ -48,7 +48,7 @@ export default function JobDetailPage(){
     return(<div className='relative overflow-hidden w-full min-h-screen flex flex-col justify-start items-center'>
         <Navbar />
         <JobDetail job={job} />
-        <div className={`absolute bg-blue-400 blur-3xl opacity-30 md:h-[400px] md:w-[400px] h-[200px] w-[200px] rounded-full -z-10 md:-right-[200px] md:bottom-[900px] -right-[100px] bottom-[1100px]`}></div>
+        <div className={`absolute bg-blue-400 blur-3xl opacity-30 md:h-[400px] md:w-[400px] h-[200px] w-[200px] rounded-full -z-10 md:-right-[200px] md:bottom-[900px] -right-[100px] bottom-[1350px]`}></div>
         <Footer />
         <div className={`absolute bg-blue-400 blur-3xl opacity-30 md:h-[400px] md:w-[400px] h-[200px] w-[200px] rounded-full -z-10 md:-left-[200px] md:-bottom-[100px] -left-[100px] -bottom-[50px]`}></div>
     </div>)
