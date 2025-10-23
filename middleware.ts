678b73f520f25d096025ff87bcf604cc5d2ddc91
221fa04732 <221fa04732@gmail.com>
2025-10-23 13:57:50 +0530

    change middleware logic

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,10 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import jwt from 'jsonwebtoken'
 
 export async function middleware(request: NextRequest){
     try{
         const token = request.cookies.get("token")?.value as string;
-        const data = jwt.verify(token, process.env.JWT_SECRET as string) as { password : string, secret : string }
-        if(data.secret != process.env.ADMIN_SECRET || data.password != process.env.ADMIN_PASSWORD){
+        if(token){
             return NextResponse.next()
         }
         return NextResponse.redirect(new URL('/admin/login', request.url))
