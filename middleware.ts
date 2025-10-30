76704ad717ee448b29dff04056945ffb59af2b29
221fa04732 <221fa04732@gmail.com>
2025-10-30 19:20:21 +0530

    added update job route as protected

--- middleware.ts
+++ middleware.ts
@@ -15,5 +15,5 @@ export async function middleware(request: NextRequest){
 }
 
 export const config = {
-    matcher: ["/admin/new-job", "/admin/new-news", "/admin/delete-news", "/admin/delete-job", "/admin/update-news"]
+    matcher: ["/admin/new-job", "/admin/new-news", "/admin/delete-news", "/admin/delete-job", "/admin/update-news", "/admin/update-job"]
 }
